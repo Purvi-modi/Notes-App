6498074c549e9c12bf4bd4ce62213bc6b882d9f6
Purvi-modi <purvimodi1997@gmail.com>
2020-10-28 20:52:29 +0530

    first commit notes app

---
+++ app.js
@@ -0,0 +1,84 @@
+/*
+ * @brief
+ * Arguments passing with yargs
+ * Storing, retrieving data with json
+ * Adding a note
+ * Removing a note
+ * Listing notes
+ * Reading a note
+ */
+
+
+
+
+
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',                        // add --title="first" --body="Hello world"
+    describe: 'Add a new note',
+    builder: {   
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
